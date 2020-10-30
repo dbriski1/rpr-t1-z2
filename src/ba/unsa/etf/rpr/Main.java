bc30bb0211db7a5276352d91ded93473fe821ca9
dbriski1 <dbriski1@etf.unsa.ba>
2020-10-30 09:22:13 +0100

    zavrsen zadatak

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class Main {
     public static int sumaCifara(int a){
-        int suma=0;
+        int suma = 0;
         do{
-            int b=a%10;
-            suma+=b;
-            a/=10;
-        }while(a!=0);
+            int b = a % 10;
+            suma += b;
+            a /= 10;
+        }while(a != 0);
         return suma;
     }
     public static void main(String[] args) {
-        
+        int broj;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Unesite broj: ");
+        broj = in.nextInt();
+        for (int i = 1 ; i < broj ; i++){
+            if(i % sumaCifara(i) == 0)
+                System.out.println(i);
+        }
     }
 }
